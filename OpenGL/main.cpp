bdedf4186772b8b8f033333a9128b7f507e7a4ec
Tamar Merdkhanian <tamar.merdkhanian@gmail.com>
2020-01-28 15:38:56 -0500

    green window

--- OpenGL/main.cpp
+++ OpenGL/main.cpp
@@ -1,8 +1,1 @@
 #include <iostream>
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
-
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
-}
--- OpenGL/src/window.cpp
+++ OpenGL/src/window.cpp
@@ -2,6 +2,60 @@
 // Created by Tamar M on 2020-01-28.
 //
 
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); //needed for iOS
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    // Initialize GLEW
+    glewExperimental = true; // Needed for core profile
+    if (glewInit() != GLEW_OK) {
+        std::cerr << "Failed to create GLEW" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    // Black background
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+
+    glViewport(0, 0, 800, 600);
+
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    while(!glfwWindowShouldClose(window))
+    {
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
 
 
